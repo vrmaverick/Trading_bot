2230dcd21039201295a0d9dd816046378c13e646
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-06-16 15:22:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from AlgorithmImports import *
 class CasualSkyBlueRabbit(QCAlgorithm):
 
     def Initialize(self): #called everytime a tick event occurs or bar reaches end time
-        self.SetStartDate(2022, 1, 1)
-        self.SetEndDate(2023,1,1)
+        self.SetStartDate(2021, 1, 1)
+        self.SetEndDate(2022,1,1)
         self.SetCash(100000)  # Set Strategy Cash #just for testing
         spy = self.AddEquity("SPY",Resolution.Daily)#daily data
         #AddForex()
@@ -14,12 +14,26 @@ class CasualSkyBlueRabbit(QCAlgorithm):
         self.spy = spy.Symbol #to get more info than tickers
         self.SetBenchmark("SPY")#actual index
 
-        self.entryprice = 0
+        self.entryPrice = 0
         self.period = timedelta(31) #31 days frame
         self.nextEntryTime = self.Time#start right away
 
 
     def OnData(self, data: Slice): #calle everytime  end of bar is reached or tick is over
+        if not self.spy in data:
+            return
         #slice class provides symbol value and time info
         # price = data.Bars[self.spy].Close #close price of day before
         price = data[self.spy].Close #close price of day before
+        if not self.Portfolio.Invested: #arent invested??
+            if self.nextEntryTime <= self.Time:
+                self.SetHoldings(self.spy,1)#100% of our portfolio
+                # self.MarketOrder(self.spy,int(self.Portfolio.Cash/price))#sends market order to specified symbol and quantity which can be -ve
+                self.Log('buy spy #'+ str(price)) #signal in log
+                self.entryPrice = price
+        elif self.entryPrice*1.1<price or self.entryPrice*0.9>price:#exit code thresholds 10%
+            self.Liquidate() #sell
+            self.Log('sell spy #'+str(price))
+            self.nextEntryTime = self.Time + self.period #deactivate for 30 days
+
+
