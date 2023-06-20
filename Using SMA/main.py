05953fa1f81d4e49378c1dfb5aaa3bc4e40a0c69
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-06-20 22:22:25 +0530

    Update main.py

--- Using SMA/main.py
+++ Using SMA/main.py
@@ -18,34 +18,30 @@ class AdaptableSkyBlueHornet(QCAlgorithm):
         if not self.sma.IsReady:
             return
         # Save high, low, and current price
-        hist = self.History(self.spy, timedelta(365), Resolution.Daily)
-        low = min(hist["low"])
-        high = max(hist["high"])
+        hi = self.History(self.spy, timedelta(365), Resolution.Daily)
+        l= min(hi["low"])
+        h = max(hi["high"])
         price = self.Securities[self.spy].Price
         # Go long if near high and uptrending
-        if price * 1.05 >= high and self.sma.Current.Value < price:
+        if price * 1.05 >= h and self.sma.Current.Value < price:
             if not self.Portfolio[self.spy].IsLong:
                 self.SetHoldings(self.spy, 1)
         # Go short if near low and downtrending
-        elif price * 0.95 <= low and self.sma.Current.Value > price:  
+        elif price * 0.95 <= l and self.sma.Current.Value > price:  
             if not self.Portfolio[self.spy].IsShort:
                 self.SetHoldings(self.spy, -1)
         # Otherwise, go flat
         else:
             self.Liquidate()
-        self.Plot("Benchmark", "52w-High", high)
-        self.Plot("Benchmark", "52w-Low", low)
+        self.Plot("Benchmark", "52w-High", h)
+        self.Plot("Benchmark", "52w-Low", l)
         self.Plot("Benchmark", "SMA", self.sma.Current.Value)
-
-
 class CustomSimpleMovingAverage(PythonIndicator):
-    
     def __init__(self, name, period):
         self.Name = name
         self.Time = datetime.min
         self.Value = 0
         self.queue = deque(maxlen=period)
-
     def Update(self, input):
         self.queue.appendleft(input.Close)
         self.Time = input.EndTime
